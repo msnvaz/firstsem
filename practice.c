fa789e88a4cb213d0c0718fbc23b553b67e27d2e
msnvaz <vazsandeep@gmail.com>
2023-09-26 17:01:06 +0530

    practice1

---
+++ practice.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*int main(){
+	int n,i,sum=0;
+	printf("Enter an integer : ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		sum=sum+i;
+	}
+	printf("Sum = %d",sum);
+	return 0;
+}*/
+int main(){
+	int i,arr1[50],n;
+	n=sizeof(arr1);
+	printf("n = %d\n",n);
+	printf("Enter  Numbers : ");
+//for(i=0;i<50;i++){
+//scanf("%d",&arr1[i]);
+	//}
+	
+}
